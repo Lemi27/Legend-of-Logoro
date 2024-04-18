2b4305538fad101521f3e0eb8e76d97574953022
Fawaaz Kamali Siddiqui <86437242+thuglyfenoyan@users.noreply.github.com>
2024-04-18 07:16:01 -0400

    Created frame for program
    - All directories and sub directories have been added
    - An template found on ExampleClass.txt is to be used when creating a new java file
    - The storyline.md and README.md files must be updated

---
+++ MainCharacter.java
@@ -0,0 +1,15 @@
+/********************************************************
+@author(s)          *insert here
+@date               *insert here
+@teacher            Andrew Carreiro
+@file               MainCharacter.java
+@description        *insert class description here
+********************************************************/
+
+
+public class MainCharacter
+{
+
+    
+
+}
