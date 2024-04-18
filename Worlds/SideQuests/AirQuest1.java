0b4f31b42c7f88bb288158ef0910bed04bc50d04
Fawaaz Kamali Siddiqui <86437242+thuglyfenoyan@users.noreply.github.com>
2024-04-18 07:33:00 -0400

    Created files for each directory
    - Added java classes for the Shop, Bosses, and SideQuests directory

---
+++ Worlds/SideQuests/AirQuest1.java
@@ -0,0 +1,25 @@
+/********************************************************
+@author(s)          *insert here
+@date               *insert here
+@teacher            Andrew Carreiro
+@file               ExampleClass.java
+@description        *insert class description here
+********************************************************/
+package Worlds.SideQuests;
+
+public class AirQuest1
+{
+
+    /*******************
+    exampleMethod(int x, int y)
+    @param          x
+    @param          y
+    @return         void
+    @description    *insert here
+    *******************/
+    public static void exampleMethod(int x, int y)
+    {
+        // enter code here, remember to comment 
+    }
+
+}
---
+++ Worlds/Worlds.java
@@ -0,0 +1,15 @@
+/********************************************************
+@author(s)          *insert here
+@date               *insert here
+@teacher            Andrew Carreiro
+@file               ExampleClass.java
+@description        *insert class description here
+********************************************************/
+package Worlds;
+
+public class Worlds
+{
+
+    
+
+}
