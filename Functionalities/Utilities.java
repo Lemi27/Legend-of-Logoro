a7af3631cc51f9e4e9b50af09ab45f8971a112b0
Fawaaz Kamali Siddiqui <86437242+thuglyfenoyan@users.noreply.github.com>
2024-04-18 07:25:40 -0400

    Create Utilities.java
    - Added file for basic input, slow print, visual functions
    - Added to TODO list in the file

---
+++ Functionalities/Utilities.java
@@ -0,0 +1,25 @@
+/********************************************************
+@author(s)          *insert here
+@date               *insert here
+@teacher            Andrew Carreiro
+@file               Utilities.java
+@description        Class with methods to manipulate arrays, 
+                    slow print, display inventories, etc.
+********************************************************/
+package Functionalities;
+
+public class Utilities
+{
+
+    /*****
+     * TODO:
+     *      + inputInt(minRange, maxRange)
+     *      + inputString(String[] acceptedAnswers) with restrictions and loops if conditions aren't met 
+     *      + slowPrint(interval)
+     *      + visualHealthBar(currentHP, maxHP)
+     *      + 2D array methods
+     *      + Dynamic array methods
+     *   
+    *****/
+
+}
