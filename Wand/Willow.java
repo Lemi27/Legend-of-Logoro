b43a433d34d703364f8ed385f080906c18daafb2
Destroyer884 <ahmad.nomaan@outlook.com>
2024-04-22 14:15:17 -0400

    Wand classes
    Made 10 different wands and made a "main" method for them to be called.

---
+++ Wand/Willow.java
@@ -0,0 +1,13 @@
+package Wand;
+
+class Willow extends Wand{
+
+    public Willow() 
+    {
+        super();
+        setName("Whisperwind Willow");
+        setDefense(1.25);
+        setOffense(2); 
+        setPrice(4); 
+    }
+}
