32eb8897300a090f350a0f2c4f4f3299b5e6859b
MonsterCoder11 <88680052+MonsterCoder11@users.noreply.github.com>
2024-04-21 20:26:51 -0400

    Update Utilities.java
    inputString() method
    inputInt() method
    Comments coming soon.

--- Functionalities/Utilities.java
+++ Functionalities/Utilities.java
@@ -8,8 +8,60 @@
 ********************************************************/
 package Functionalities;
 
+import java.util.Scanner;
 public class Utilities
 {
+
+    private Scanner uI;
+
+    public Utilities() {
+        this.uI = new Scanner(System.in);
+        }
+    
+        public int inputInt(String prompt, int min, int max) {
+
+            int input;
+            do{
+                System.out.println(prompt);
+                while (!uI.hasNextInt()) {
+                    System.out.println("Please enter a valid integer. ");
+                    System.out.print(prompt);
+                    uI.next();
+                }
+                input =uI.nextInt();
+                while (input< min || input > max) {
+                    System.out.println("Please enter a valid integer");
+                    System.out.print(prompt);
+                }
+            } while (input > min || input < max);
+            return input;
+        }
+    
+        public String inputString(String prompt, String[] acceptedAnswers, String input) {
+    
+            String answer;
+            boolean isValidInput;
+            do {
+                System.out.println(prompt);
+                answer = uI.next();
+                isValidInput = false;
+                for (String acceptedAnswer : acceptedAnswers) {
+                    if (answer.equals(acceptedAnswer)){
+                        isValidInput = true;
+                        break;
+                    }
+                }
+                if (!isValidInput) {
+                    System.out.println("Invalid input. Please try again. ");
+                }
+            } while (!isValidInput);
+            return answer;
+        }
+    
+        public void slowPrint(String text, double interval){
+            
+        }
+    }
 
     /*****
      * TODO:
@@ -22,4 +74,10 @@ public class Utilities
      *   
     *****/
 
+}
+    class RolePlayingGame{
+        public static void main(String [] args) {
+    
+        }
+    
 }
