424f661a219094d4dc7ad51756ed3ced392574e1
Destroyer884 <114109307+Destroyer884@users.noreply.github.com>
2024-04-23 13:09:44 -0400

    Wand classes (#5)
    Made 10 different wands and made a "main" method for them to be called.

---
+++ Wand/Ebony.java
@@ -0,0 +1,13 @@
+package Wand;
+
+class Ebony extends Wand{
+    
+    public Ebony()
+    {
+        super(); 
+        setName("Shadowed Ebonyreach"); 
+        setDefense(1); 
+        setOffense(1); 
+        setPrice(1); 
+    }
+}
---
+++ Wand/Spruce.java
@@ -0,0 +1,13 @@
+package Wand;
+
+class Spruce extends Wand {
+    
+    public Spruce()
+    {
+        super(); 
+        setName("Sapphire Sprig"); 
+        setDefense(2); 
+        setOffense(2); 
+        setPrice(7); 
+    }
+}
