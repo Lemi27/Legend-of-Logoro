ff9a1e729db1700a8d78cfdb1f43373f773dcf8f
Lemi Miyu <lemimiyu0@gmail.com>
2024-04-24 12:03:38 -0400

    Boss Changes (#12)
    - Completed Boss Parent class
    - Set variables in child boss classes (not final)
    - comments to come

---
+++ src/Worlds/Bosses/AirBoss.java
@@ -0,0 +1,19 @@
+package src.Worlds.Bosses;
+
+public class AirBoss extends Bosses {
+
+    public AirBoss() {
+
+        setHp(8);
+        setOffense(8);
+        bossWand.setDefense(8);
+        bossWand.setOffense(8);
+        setLevel(1);
+        setMinAttack(9);
+        setMinDefense(9);
+        setMaxAttack(9);
+        setMaxDefense(9);
+
+    }
+    
+}
---
+++ src/Worlds/Bosses/FireBoss.java
@@ -0,0 +1,19 @@
+package src.Worlds.Bosses;
+
+public class FireBoss extends Bosses {
+
+    public FireBoss() {
+
+        setHp(8);
+        setOffense(8);
+        bossWand.setDefense(8);
+        bossWand.setOffense(8);
+        setLevel(4);
+        setMinAttack(9);
+        setMinDefense(9);
+        setMaxAttack(9);
+        setMaxDefense(9);
+
+    }
+    
+}
